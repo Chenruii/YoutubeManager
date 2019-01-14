ea0b408b3e88a1b1a85cfc26e72ce8dc4db2617a
Chenruii <rui.chen1996@gmail.com>
2019-01-14 23:27:51 +0100

    login

--- src/Form/LoginUserType.php
+++ src/Form/LoginUserType.php
@@ -1,15 +1,30 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: ruich
- * Date: 14/01/2019
- * Time: 23:15
- */
 
-namespace App\Controller;
+namespace App\Form;
 
+use App\Entity\User;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 
-class LoginUserType
+class LoginUserType extends AbstractType
 {
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('email',EmailType::class)
+            ->add('password',PasswordType::class)
+            ->add('submit',SubmitType::class)
+        ;
+    }
 
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => User::class,
+        ]);
+    }
 }
