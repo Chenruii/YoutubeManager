cd0e7a85b76765ac3b6aa94b8bf6656e8d8ce4bd
Chenruii <rui.chen1996@gmail.com>
2019-01-27 14:23:36 +0100

    suite

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -5,11 +5,10 @@ namespace App\Controller;
 use App\Entity\User;
 use App\Form\UserType;
 use App\Repository\UserRepository;
+use Doctrine\ORM\EntityManager;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\HttpFoundation\Request;
-use Sensio\Bundle\FrameworkExtraBundle\Configuration\ParamConverter;
-
+use Symfony\Component\Routing\Annotation\Route;
 
 
 
@@ -18,22 +17,19 @@ class UserController extends AbstractController
     /**
      * @Route("/user", name="user")
      */
-    public function index(Request $request,UserRepository $userRepository)
+    public function index(Request $request, UserRepository $userRepository)
     {
-
-        $user = new User();
-
-        $form = $this->createForm(UserType::class ,$user);
+        $user  = new User();
+        $form = $this->createForm(UserType::class, $user);
         $form->handleRequest($request);
 
-        if ($form->isSubmitted() && $form->isValid()){
+        if ($form->isSubmitted() && $form->isValid()) {
             $entityManager = $this->getDoctrine()->getManager();
-            $entityManager->persist($user);
-            $entityManager->flush();
+            $entityManager->persist($user);    // file attente
+            $entityManager->flush();            // execute file attente
             // type can be anything, for example : notice, warning, error...
             $this->addFlash('notice', 'Your changes were saved!');
-            return $this->redirectToRoute('user');
-
+            return $this->redirectToRoute('home');
         }
 
         $users = $userRepository->findAll() ;
@@ -43,23 +39,37 @@ class UserController extends AbstractController
             'users' =>$users,
         ));
     }
+
     /**
-     * @Route("/user", name="liste_user")
+     * @Route("/user", name="list_user")
      */
-    public function liste(UserRepository $userRepository)
+    public function liste(UserRepository $userRepository,User $user)
     {
-        return $this->render( 'home/index.html.twig', [
+        return $this->render( 'home/index.html.twig',array(
             'users' => $userRepository->findAll(),
-        ]);
+        ));
     }
 
     /**
      * @Route("/user/{byFirstname}", name="user_firstname")
-     * @ParamConverter("user", options={"mapping"={"byFirstname"="firstname"}})
      *
      */
     public function firstname(Request $request, UserRepository $userRepository, User $user)
     {
         return $this->render('home/index.html.twig');
+    }
+
+    /**
+     * @Route("/user/remove/{id}", name="remove_user")
+     */
+    public function remove(User $user, EntityManager $entityManager)
+    {
+        $videos = $user->getVideos();
+        foreach ($videos as $video){
+            $video->setUser(null);
+        }
+        $entityManager->remove($user);
+        $entityManager->flush();
+        return $this->redirectToRoute('home');
     }
 }
