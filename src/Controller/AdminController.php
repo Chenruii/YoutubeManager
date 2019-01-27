0aeb63b18bd6c7848a69f3399c5e36dcf914ae0c
Chenruii <rui.chen1996@gmail.com>
2019-01-27 14:20:15 +0100

    adminController

--- src/Controller/AdminController.php
+++ src/Controller/AdminController.php
@@ -2,7 +2,10 @@
 
 namespace App\Controller;
 
+use App\Repository\UserRepository;
+use App\Repository\VideoRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 
 class AdminController extends AbstractController
@@ -10,10 +13,11 @@ class AdminController extends AbstractController
     /**
      * @Route("/admin", name="admin")
      */
-    public function index()
+    public function index(Request $request,UserRepository $userRepository,VideoRepository $videoRepository)
     {
         return $this->render('admin/index.html.twig', [
-            'controller_name' => 'AdminController',
+            'users' => $userRepository->findAll(),
+            'videos' => $videoRepository->findAll(),
         ]);
     }
 }
